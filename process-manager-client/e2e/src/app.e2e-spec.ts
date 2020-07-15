d43ed8d6e1b0c02cb71b7d16c3a07f50a22e376f
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2020-07-15 15:33:20 +0900

    [update]名前変更

--- process-manager-client/e2e/src/app.e2e-spec.ts
+++ process-manager-client/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('process-manager app is running!');
+    expect(page.getTitleText()).toEqual('process-manager-client app is running!');
   });
 
   afterEach(async () => {
