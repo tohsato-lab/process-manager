65455a3cf8ef4873dd9efd669c510cce7277b7e6
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2020-07-16 01:16:59 +0900

    [update]アップロード、展開、実行成功

--- main.go
+++ main.go
@@ -1,64 +1,13 @@
 package main
 
 import (
-	"./utils"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"mime/multipart"
 	"net/http"
 	"os"
+
+	"./api"
 )
-
-type response struct {
-	Status string `json:"status"`
-	Data   string `json:"data"`
-}
-
-func uploadHander(w http.ResponseWriter, r *http.Request) {
-	// このハンドラ関数へのアクセスはPOSTメソッドのみ認める
-	if r.Method != "POST" {
-		fmt.Fprintln(w, "許可したメソッドとはことなります。")
-		return
-	}
-	var file multipart.File
-	var fileHeader *multipart.FileHeader
-	var e error
-	var uploadedFileName string
-	// POSTされたファイルデータを取得する
-	file, fileHeader, e = r.FormFile("file")
-	fmt.Printf("%T\n", file)
-	if e != nil {
-		fmt.Fprintln(w, "ファイルアップロードを確認できませんでした。")
-		return
-	}
-	uploadedFileName = fileHeader.Filename
-	var saveFile *os.File
-	saveFile, e = os.Create("./" + uploadedFileName)
-	if e != nil {
-		fmt.Fprintln(w, "サーバ側でファイル確保できませんでした。")
-		return
-	}
-	defer saveFile.Close()
-	defer file.Close()
-	_, e = io.Copy(saveFile, file)
-	if e != nil {
-		fmt.Println(e)
-		fmt.Println("アップロードしたファイルの書き込みに失敗しました。")
-		os.Exit(1)
-	}
-	// unzip
-	utils.Unzip("./"+uploadedFileName, "output")
-	// return
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	response := response{
-		Status: "ok",
-		Data:   "test",
-	}
-	json, _ := json.Marshal(response)
-	w.Write(json)
-}
 
 func main() {
 	if err := os.Mkdir("dataset", 0777); err != nil {
@@ -70,6 +19,6 @@ func main() {
 	if err := os.Mkdir("logs", 0777); err != nil {
 		fmt.Println(err)
 	}
-	http.HandleFunc("/upload", uploadHander)
+	http.HandleFunc("/upload", api.UploadHander)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
