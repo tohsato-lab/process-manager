b563ac08a136a3d7aa45efd42155226e2f298cf3
hirorittsu <zq6295migly@gmail.com>
2020-07-15 17:51:08 +0900

    [update]ドラッグからアップロードまで確認

--- process-manager-client/src/app/app.component.ts
+++ process-manager-client/src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'process-manager-client';
+
+  title = 'dropzone';
+  files: File[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  public onSelect(event): void {
+    console.log(event);
+    this.files.push(...event.addedFiles);
+    const formData = new FormData();
+    for (const file of this.files) {
+      formData.append('file', file);
+    }
+
+    this.upload(formData);
+
+  }
+
+  public onRemove(event): void {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+
+  }
+
+  private upload(formData): void {
+    console.log('upload');
+    /*
+    if (files.length <= 0) {
+      return;
+    }
+
+    const file = files[0];
+    const formData = new FormData();
+
+    formData.append('file', file, file.name);
+     */
+
+    this.http.post('http://localhost:8081/upload', formData)
+      .subscribe(value => {
+        console.log(value);
+      });
+  }
+
 }
