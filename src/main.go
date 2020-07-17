45a6f9c81686bdb5205048480db9ac2a7b744764
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2020-07-17 15:55:38 +0900

    [update]データベース操作追加

--- src/main.go
+++ src/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"./api"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+// Database データベース操作用
+type Database struct {
+	Val string
+}
 
 func main() {
 	if err := os.Mkdir("../dataset", 0777); err != nil {
@@ -19,6 +26,15 @@ func main() {
 	if err := os.Mkdir("../logs", 0777); err != nil {
 		fmt.Println(err)
 	}
-	http.HandleFunc("/upload", api.UploadHander)
+	db, err := sql.Open("mysql", "golang:golang@/process_manager_db?parseTime=true")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		api.UploadHander(w, r, db)
+	})
+
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
