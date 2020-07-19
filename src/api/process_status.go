4af16ee6effaef156ee29ef452e35b4d612db625
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2020-07-19 12:33:39 +0900

    [update]websocket接続確認

--- src/api/process_status.go
+++ src/api/process_status.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -10,19 +9,28 @@ import (
 )
 
 // WebSocket 更新用
-var upgrader = websocket.Upgrader{}
+// request origin not allowed by Upgrader.CheckOrigin エラーを突破
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	// Add this lines
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 // WebSocket サーバーにつなぎにいくクライアント
 var clients = make(map[*websocket.Conn]bool)
 
 // ProcessStatusHandle プロセス一覧をリアルタイムで返す
 func ProcessStatusHandle(w http.ResponseWriter, r *http.Request) {
-
 	// websocket の状態を更新
+
 	websocket, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Fatal("error upgrading GET request to a websocket::", err)
 	}
+
 	// websocket を閉じる
 	defer websocket.Close()
 
@@ -31,7 +39,6 @@ func ProcessStatusHandle(w http.ResponseWriter, r *http.Request) {
 	for {
 		// メッセージ受け取り
 		process := <-utils.BroadcastProcess
-		fmt.Println("send")
 
 		// クライアントの数だけループ
 		for client := range clients {
