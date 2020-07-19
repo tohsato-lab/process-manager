b9869b829a93f7e9ace0405a17b8fddeabfec6c1
hirorittsu <zq6295migly@gmail.com>
2020-07-19 23:59:14 +0900

    [update]ページ遷移ベース確認

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,38 @@ import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {NgxDropzoneModule} from 'ngx-dropzone';
 import {FormsModule} from '@angular/forms';
-import { HomeComponent } from './home/home.component';
-import { HistoryComponent } from './history/history.component';
-import { DatasetComponent } from './dataset/dataset.component';
+import {HomeComponent} from './home/home.component';
+import {HistoryComponent} from './history/history.component';
+import {DatasetComponent} from './dataset/dataset.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'dataset', component: DatasetComponent},
+    {path: 'history', component: HistoryComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    HistoryComponent,
-    DatasetComponent
-  ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        HistoryComponent,
+        DatasetComponent
+    ],
 
     imports: [
         BrowserModule,
         HttpClientModule,
         NgxDropzoneModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forRoot(routes),
     ],
 
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 
 export class AppModule {
 }
