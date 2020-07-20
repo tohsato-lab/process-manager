0263348061a1100559a09deb17a83f84bd426195
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2020-07-20 14:58:06 +0900

    [update]メニュー更新

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    public onHome(): void {
+        this.router.navigate(['home']);
+    }
+
+    public onDataset(): void {
+        this.router.navigate(['dataset']);
+    }
+
+    public onHistory(): void {
+        this.router.navigate(['history']);
+    }
 
 }
