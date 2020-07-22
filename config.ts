84fe44a52896e45cdc707eac32635e31c14868a1
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2020-07-22 23:22:15 +0900

    [update]接続エラーの表示実装

--- config.ts
+++ config.ts
@@ -1,7 +1,8 @@
 const config = {
     httpScheme: 'https://',
     websocketScheme: 'wss://',
-    host: '133.19.106.94',
+    // host: '133.19.106.94',
+    host: 'localhost',
     port: '5983'
 };
 
