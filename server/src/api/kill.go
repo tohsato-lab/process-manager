ccf7362b197e0c2398289d6b59be42c295a3c383
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2020-07-26 00:22:02 +0900

    [update]null回避

--- server/src/api/kill.go
+++ server/src/api/kill.go
@@ -17,22 +17,31 @@ func KillHander(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 
 	// pid取得
 	var pid = 0
-	err := db.QueryRow("SELECT pid FROM process_table WHERE id = ?", id).Scan(&pid)
+	err := db.QueryRow("SELECT IFNULL(pid, 0) FROM process_table WHERE id = ?", id).Scan(&pid)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// 親子共々kill
-	exec.Command("sh", "-c", "kill `ps ho pid --ppid="+strconv.Itoa(pid)+"`").Run()
-	exec.Command("kill", strconv.Itoa(pid)).Run()
-
-	modules.ComplateProcess(db, id, "killed")
+	status := "killed"
+	if pid != 0 {
+		// 親子共々kill
+		if err := exec.Command("sh", "-c", "kill `ps ho pid --ppid="+strconv.Itoa(pid)+"`").Run(); err != nil {
+			panic(err.Error())
+		}
+		if err := exec.Command("kill", strconv.Itoa(pid)).Run(); err != nil {
+			panic(err.Error())
+		}
+		// DBアップデート
+		modules.ComplateProcess(db, id, status)
+	} else {
+		status = "not kill"
+	}
 
 	// return
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	response := response{
 		Status: "200",
-		Data:   "killed",
+		Data:   status,
 	}
 	json, _ := json.Marshal(response)
 	w.Write(json)
