6eabe1cd248fd21ff55c50f8c6c2c2ebcb47541b
hirorittsu <zq6295migly@gmail.com>
2020-07-31 00:40:03 +0900

    [update]時間表記をフォーマット

--- server/src/utils/data.go
+++ server/src/utils/data.go
@@ -1,17 +1,13 @@
 package utils
 
-import (
-	"time"
-)
-
 //Process is struct
 type Process struct {
 	ID           string
 	UseVram      float32
 	Status       string
 	Filename     string
-	StartDate    *time.Time
-	CompleteDate *time.Time
+	StartDate    string
+	CompleteDate string
 }
 
 // BroadcastProcess データベースの情報を格納する
