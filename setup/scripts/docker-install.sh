6adb10e5ec55b158b813281a464d12d616691b29
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2020-07-31 22:50:59 +0900

    [update]composeスクリプト実装

--- setup/scripts/docker-install.sh
+++ setup/scripts/docker-install.sh
@@ -1,3 +1,7 @@
+# install docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # install nvidia runtime
 curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
   sudo apt-key add -
--- setup/scripts/npm-install.sh
+++
@@ -1,6 +0,0 @@
-apt update
-apt install -y nodejs npm
-npm install n -g
-n stable
-apt purge -y nodejs npm
-exec $SHELL -l
