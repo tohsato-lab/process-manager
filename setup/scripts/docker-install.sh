8375631ea5e4b02edf3606b08bf2b9d33cd0f311
hirorittsu <zq6295migly@gmail.com>
2020-07-31 17:04:17 +0900

    [add]setupスクリプト作成開始

---
+++ setup/scripts/docker-install.sh
@@ -0,0 +1,10 @@
+# install nvidia runtime
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+  sudo apt-key add -
+ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+ curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+ sudo apt update
+ sudo apt install nvidia-container-runtime
+ sudo cp data/daemon.json /etc/docker/daemon.json
+ sudo reboot
