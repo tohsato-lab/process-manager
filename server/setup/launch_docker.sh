d91253bf29c22b99be80d933988cadb125cdea1e
hirorittsu <zq6295migly@gmail.com>
2020-08-01 14:26:08 +0900

    [update]compose修正

--- server/setup/launch_docker.sh
+++ server/setup/launch_docker.sh
@@ -1,5 +1,5 @@
 set -e
 
 sudo docker build -t process_manager .
-sudo docker run -v /home/migly/LAB/data:/process-manager-server/dataset:ro --gpus all -it --privileged -d -p 5983:5983 -p 3316:3306 --name server process_manager:latest /bin/bash
+sudo docker run -v /home/migly/LAB/data:/process-manager/dataset:ro --gpus all -it --privileged -d -p 5983:5983 -p 3316:3306 -p 8080:8080 --name server process_manager:latest /bin/bash
 sudo docker exec -it server /bin/bash
