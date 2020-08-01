042f6b51d791b71c6fc7e147c4f366b8b907a876
hirorittsu <zq6295migly@gmail.com>
2020-08-01 13:24:31 +0900

    [upd[update]mysql接続変更

--- setup/scripts/docker-install.sh
+++ setup/scripts/docker-install.sh
@@ -1,3 +1,16 @@
+# install docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt install docker-ce
+
+# docker 権限付与
+sudo gpasswd -a $(whoami) docker
+sudo chgrp docker /var/run/docker.sock
+sudo chmod 666 /var/run/docker.sock
+sudo service docker restart
+
 # install docker compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
