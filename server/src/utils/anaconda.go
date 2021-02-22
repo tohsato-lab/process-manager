705e2f828dae82d6f9bf7cfc2a65c1363eb91240
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2021-02-22 15:16:59 +0900

    [update]環境取得api修正

--- server/src/utils/anaconda.go
+++ server/src/utils/anaconda.go
@@ -8,10 +8,11 @@ import (
 
 // GetCondaEnv Anacondaのenv一覧を取得
 func GetCondaEnv() []string {
-	cmdstr := "conda info -e | grep anaconda3 | awk '{print $1}'"
+	cmdstr := "conda info -e | grep 'conda' | grep -v '#' | awk '{print $1}'"
 	out, err := exec.Command("sh", "-c", cmdstr).Output()
 	if err != nil {
 		fmt.Println("Command Exec Error.")
 	}
-	return strings.Split(string(out), "\n")[:4]
+	envArray := strings.Split(string(out), "\n")
+	return envArray[:len(envArray)-1]
 }
