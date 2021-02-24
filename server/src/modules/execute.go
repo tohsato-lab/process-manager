3997c823801a40d4f5101b0b7cb5324c3519cb86
hirorittsu <zq6295migly@gmail.com>
2021-02-24 14:14:06 +0900

    [fix]実行周りのエラーハンドリング修正

--- server/src/modules/execute.go
+++ server/src/modules/execute.go
@@ -2,6 +2,7 @@ package modules
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 	"os/exec"
 	"strconv"
@@ -13,19 +14,16 @@ func Execute(db *sql.DB, id string, targetFile string, envName string) string {
 	cmd := exec.Command("bash", "execute.sh", "../../data/programs/"+id, targetFile, envName)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Start()
+	if err := cmd.Start(); err != nil {
+		return "unknown:" + err.Error()
+	}
 
 	// PID登録
-	statusUpdate, err := db.Prepare("UPDATE process_table SET pid=? WHERE id=?")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer statusUpdate.Close()
-	if _, err := statusUpdate.Exec(strconv.Itoa(cmd.Process.Pid), id); err != nil {
-		panic(err.Error())
-	}
+	RegisterPID(db, id, cmd.Process.Pid)
 
-	cmd.Wait()
+	if err := cmd.Wait(); err != nil {
+		fmt.Println(err)
+	}
 
 	status := ""
 	signal := cmd.ProcessState.ExitCode()
@@ -34,7 +32,7 @@ func Execute(db *sql.DB, id string, targetFile string, envName string) string {
 		status = "complete"
 	case 1:
 		status = "error"
-	case 143:
+	case 128 + 15:
 		status = "killed"
 	default:
 		status = "unknown:" + strconv.Itoa(signal)
