12a2a087e57b79fb85da69ac7d20b0abc137b5da
HiroRittsu <HiroRittsu@users.noreply.github.com>
2021-03-07 17:22:50 +0900

    [update]local-installスクリプト追加

---
+++ setup/local-install.sh
@@ -0,0 +1,16 @@
+# angular
+sudo apt install -y nodejs npm
+sudo npm install n -g
+sudo n stable
+sudo apt purge -y nodejs npm
+exec $SHELL -l
+sudo npm install -g @angular/cli
+
+
+# golang
+sudo apt install golang-go
+
+#mysql
+sudo apt install mysql-server mysql-client
+sudo mysql_secure_installation
+
