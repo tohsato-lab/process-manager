544ca3561f2bd14ffacc99cb4028a35f00f2a076
HiroRittsu <HiroRittsu@users.noreply.github.com>
2021-03-07 18:30:46 +0900

    [update]フィアルサーチ追加してみた

--- server/src/api/exproler.go
+++ server/src/api/exproler.go
@@ -2,11 +2,20 @@ package api
 
 import (
 	"database/sql"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 // Explorer ディレクトリの閲覧
 func Explorer(w http.ResponseWriter, r *http.Request, _ *sql.DB) {
 	println(r.URL.Path[1:])
+	files, err := ioutil.ReadDir("../../data/" + r.URL.Path[1:])
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
 	http.ServeFile(w, r, "../../data/"+r.URL.Path[1:])
 }
