69213ce08fb87499e7059ab1a638a10699b082bf
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2021-06-10 22:19:56 +0900

    [update]table整理完了

--- server/src/utils/data.go
+++ server/src/utils/data.go
@@ -3,15 +3,14 @@ package utils
 //Process is struct
 type Process struct {
 	ID           string
-	UseVram      float32
 	Status       string
 	Filename     string
 	StartDate    string
 	CompleteDate string
 	TargetFile   string
 	EnvName      string
-	ExecCount    int32
 	Comment      string
+	InTrash      bool
 }
 
 type DirectoryInfo struct {
