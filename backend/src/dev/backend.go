b1bbe845acd8bcafb2853ccf33f7c7a005715a41
HiroRittsu <33301907+HiroRittsu@users.noreply.github.com>
2021-11-24 17:13:12 +0900

    [add]プロトタイプ実装

---
+++ backend/src/dev/backend.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+
+func register(w http.ResponseWriter, r *http.Request) {
+	go func() {
+		u := url.URL{Scheme: "ws", Host: ":8100", Path: "/connect", RawQuery: "value=test"}
+		log.Printf("connecting to %s", u.String())
+		c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+		if err != nil {
+			log.Fatal("dial:", err)
+		}
+		defer func(c *websocket.Conn) {
+			if c.Close() != nil {
+				return
+			}
+		}(c)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				log.Println("disconnect")
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+}
+
+func main() {
+	http.HandleFunc("/register", register)
+	log.Fatal(http.ListenAndServe(":8101", nil))
+}
--- backend/src/dev/conda.go
+++ backend/src/dev/conda.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 var upgrader = websocket.Upgrader{} // use default options
 
 func connect(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.FormValue("value"))
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
@@ -34,29 +36,6 @@ func connect(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// http.HandleFunc("/connect", connect)
-	// log.Fatal(http.ListenAndServe(":8100", nil))
-
-	a := make(chan int)
-	b := make(chan int)
-	c := make(chan int)
-
-	abc := make(map[chan int]bool)
-	abc[a] = true
-	abc[b] = true
-	abc[c] = true
-
-	go func() { for { a <- 0 } }()
-	go func() { for { <-c } }()
-	go func() { for { b <- 0 } }()
-
-	for i := 0; i < 10; i++ {
-		for ints := range abc {
-			select {
-			case <-ints:
-				println("debug")
-			}
-		}
-	}
-
+	http.HandleFunc("/connect", connect)
+	log.Fatal(http.ListenAndServe(":8100", nil))
 }
